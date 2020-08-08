770d1bd13ed46b0b434f9f27e82708091db63d38
Imran Khan <coadingbird@gmail.com>
2020-08-08 13:07:59 +0600

    first class complete

---
+++ TruthyAndFalsyValues.js
@@ -0,0 +1,17 @@
+// const age = 0;
+
+// const name = "0";
+// falsy
+// 0
+// ''
+// undefined
+// null
+// NaN
+// false
+let name = {};
+console.log(name);
+if (name) {
+  console.log("Condition is true");
+} else {
+  console.log("Condition is false");
+}
