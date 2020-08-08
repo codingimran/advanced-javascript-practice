155bd740d5f0b6e92ee162d554720c645c43e619
Imran Khan <coadingbird@gmail.com>
2020-08-08 13:41:53 +0600

    2nd class...null vs undefined

---
+++ nullVsUndefined.js
@@ -0,0 +1,25 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+//   return;
+// }
+
+// const result = add(5, 55);
+// console.log(result);
+
+// function add(n1, n2) {
+//   return n1, n2;
+// }
+// const result = add(33);
+// console.log(result);
+
+// const imran = { name: "Imran Khan", phone: 545454545 };
+// console.log(imran.gf);
+
+// let fun = undefined;
+// console.log(fun);
+
+let ages = [3, 5, 43];
+console.log(ages[5]);
